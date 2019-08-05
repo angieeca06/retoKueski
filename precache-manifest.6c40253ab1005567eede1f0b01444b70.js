b81f94343d462c14ec6d90e6ecf0c651899421b8
angie <angyalva16@gmail.com>
2019-08-05 10:44:07 -0500

    Updates

---
+++ precache-manifest.6c40253ab1005567eede1f0b01444b70.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "55278efa204a7005e2d28a4de1ed0300",
+    "url": "/retokueski/index.html"
+  },
+  {
+    "revision": "d260ff45b81ce61c89d2",
+    "url": "/retokueski/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/retokueski/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "d260ff45b81ce61c89d2",
+    "url": "/retokueski/static/js/main.11c76c80.chunk.js"
+  },
+  {
+    "revision": "588c423a11a78a7b8321",
+    "url": "/retokueski/static/js/runtime~main.efda95fb.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/retokueski/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/retoKueski/precache-manifest.baf49ca72fd97b4c425eda19f707089c.js"
+  "/retokueski/precache-manifest.6c40253ab1005567eede1f0b01444b70.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/retoKueski/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/retokueski/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
