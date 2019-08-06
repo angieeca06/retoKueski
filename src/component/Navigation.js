d6d8dc2e85985d601fbb0bb15cdfdf5a7280905e
angie <angyalva16@gmail.com>
2019-08-06 15:57:11 -0500

    Delete dead code

--- src/component/Navigation.js
+++ src/component/Navigation.js
@@ -1,21 +1,20 @@
-import React from 'react';
-import {Navbar, Button} from 'react-bootstrap';
-import {Link} from 'react-router-dom';
+import React from "react";
+import { Navbar, Button } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
 const Navigation = () => {
-    const LogOut = () => {
-        localStorage.clear();
-        console.log("sesion cerrada");
-    }
-    return (
-        <Navbar bg="light" expand="lg">
-            <Navbar.Brand>Weeber</Navbar.Brand>
-            <Navbar.Toggle aria-controls="basic-navbar-nav" />
-            <Link to='/'>
-                <Button onClick={LogOut}>Cerrar sesión</Button>
-            </Link>
-        </Navbar>
-    )
-}
+  const LogOut = () => {
+    localStorage.clear();
+  };
+  return (
+    <Navbar bg="light" expand="lg">
+      <Navbar.Brand>Weeber</Navbar.Brand>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Link to="/">
+        <Button onClick={LogOut}>Cerrar sesión</Button>
+      </Link>
+    </Navbar>
+  );
+};
 
-export default Navigation 
+export default Navigation;
--- src/component/Register.js
+++ src/component/Register.js
@@ -1,147 +1,149 @@
-import React, {useState} from 'react';
-import {Form, Button} from 'react-bootstrap';
-import {Link} from 'react-router-dom';
-import { FaArrowRight } from 'react-icons/fa';
-import styled from 'styled-components';
+import React, { useState } from "react";
+import { Form, Button } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import { FaArrowRight } from "react-icons/fa";
+import styled from "styled-components";
 
 const Styles = styled.div`
-background-color: #f1f1f1;
+  background-color: #f1f1f1;
 
-.welcomeImg{
-  display: block;
-  margin: auto;
-}
+  .welcomeImg {
+    display: block;
+    margin: auto;
+  }
 
-p {
-  margin: 2rem 1rem 1rem 1rem;
-}
+  p {
+    margin: 2rem 1rem 1rem 1rem;
+  }
 
-form {
-  background-color: #FFFFFF;
-  margin: 2rem 1rem 1rem 1rem;
-  padding: 1rem;
-  box-shadow: 7px 8px 7px rgba(3, 3, 3, 0.4);
+  form {
+    background-color: #ffffff;
+    margin: 2rem 1rem 1rem 1rem;
+    padding: 1rem;
+    box-shadow: 7px 8px 7px rgba(3, 3, 3, 0.4);
 
-  input {
-    border-left: none;
-    border-right: none;
-    border-top: none;
-    border-bottom-width: 1px;
-    border-color: rgba(3, 3, 3, 0.4);
+    input {
+      border-left: none;
+      border-right: none;
+      border-top: none;
+      border-bottom-width: 1px;
+      border-color: rgba(3, 3, 3, 0.4);
 
-    ::placeholder{
-      color: rgba(3, 3, 3, 0.4)
+      ::placeholder {
+        color: rgba(3, 3, 3, 0.4);
+      }
+    }
+
+    h1 {
+      background-color: #ffffff;
+      font-style: bold;
+      color: #707070;
+    }
+
+    .form-group {
+      background-color: #ffffff;
+      text-align: center;
     }
   }
 
-  h1 {
-    background-color: #FFFFFF;
-    font-style: bold;
-    color: #707070;
-  }
-
-  .form-group {
-    background-color: #FFFFFF;
+  .registro {
+    border: none;
+    float: right;
+    color: white;
+    font-size: 2rem;
+    background-color: #f72525;
+    padding: 20px;
     text-align: center;
+    border-radius: 3rem;
   }
-}
 
-.registro {
-  border: none;
-  float: right;
-  color: white;
-  font-size: 2rem;
-  background-color: #F72525;
-  padding: 20px;
-  text-align: center;
-  border-radius: 3rem;
-}
-
-.space{
-  margin: 5rem 1rem 1rem 1rem;
-  color: #f1f1f1;
-}
-
-.login {
-  border: none;
-  font-size: 1.2rem;
-  color: white;
-  background-color: #F72525;
-  padding: 20px;
-  text-align: center;
-  display: block;
-  margin: auto;
-  border-radius: 3rem;
-
-  a {
-    background-color: none;
+  .space {
+    margin: 5rem 1rem 1rem 1rem;
+    color: #f1f1f1;
   }
-}
 
-`
+  .login {
+    border: none;
+    font-size: 1.2rem;
+    color: white;
+    background-color: #f72525;
+    padding: 20px;
+    text-align: center;
+    display: block;
+    margin: auto;
+    border-radius: 3rem;
+
+    a {
+      background-color: none;
+    }
+  }
+`;
 const Register = props => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
-  const handleChange = (event) => {
-        if (event.target.name === "email") {
-            setEmail(event.target.value);
-        }
-        if (event.target.name === "password") {
-            setPassword(event.target.value);
-        }
-    };
-
-    function register() {
-      fetch("/api/user/register", {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({ email, password })
-      })
-        .then(response => {
-          return response.json();
-        })
-        .then(response => {
-          console.log("sesion activa");
-          localStorage.setItem("token", JSON.stringify(response));
-        });
+  const handleChange = event => {
+    if (event.target.name === "email") {
+      setEmail(event.target.value);
     }
-  return(
+    if (event.target.name === "password") {
+      setPassword(event.target.value);
+    }
+  };
+
+  function register() {
+    fetch("/api/user/register", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ email, password })
+    })
+      .then(response => {
+        return response.json();
+      })
+      .then(response => {
+        localStorage.setItem("token", JSON.stringify(response));
+      });
+  }
+  return (
     <React.Fragment>
       <Styles>
-      <img src="https://i.imgur.com/NtRT2Fy.jpg" alt="Captura tus ideas y conviértelas en metas. Weeber te ayuda a volverlas realidad" className="welcomeImg" />
-      <Form>
-        <h1>¡Regístrate!</h1>
-        <Form.Group controlId="formBasicEmail">
+        <img
+          src="https://i.imgur.com/NtRT2Fy.jpg"
+          alt="Captura tus ideas y conviértelas en metas. Weeber te ayuda a volverlas realidad"
+          className="welcomeImg"
+        />
+        <Form>
+          <h1>¡Regístrate!</h1>
+          <Form.Group controlId="formBasicEmail">
+            <Form.Control
+              onChange={handleChange}
+              type="email"
+              name="email"
+              placeholder="E-mail"
+            />
+          </Form.Group>
+          <Form.Group controlId="formBasicPassword">
+            <Form.Control
+              onChange={handleChange}
+              type="password"
+              name="password"
+              placeholder="Contraseña"
+            />
+          </Form.Group>
+          <Link>
+            <Button onClick={register} variant="primary" className="registro">
+              <FaArrowRight />
+            </Button>
+          </Link>
+        </Form>
 
-          <Form.Control  onChange={handleChange} type="email" name="email" placeholder="E-mail" />
-        </Form.Group>
-        <Form.Group controlId="formBasicPassword">
-
-          <Form.Control onChange={handleChange} type="password" name="password" placeholder="Contraseña" />
-        </Form.Group>
-        <Link>
-        <Button onClick={register} variant="primary" className="registro">
-          <FaArrowRight />
-        </Button>
-        </Link>
-      </Form>
-
-      <p className="space">.</p>
+        <p className="space">.</p>
 
         <Button className="login">
-          <Link to="/"> 
-            ¿Ya tienes una cuenta?
-          </Link>
+          <Link to="/">¿Ya tienes una cuenta?</Link>
         </Button>
-    </Styles>
+      </Styles>
     </React.Fragment>
-
-
-
-  )
-
-}
-
-
- export default Register;
+  );
+};
+export default Register;
